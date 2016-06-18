1b083dc083fa45c4bed58c95822736ac603ab2fc
Clement <clemetsoullard@yahoo.fr>
2016-06-18 05:47:43 +0000

    La télé fonctionne

--- scheduler.c
+++ scheduler.c
@@ -24,74 +24,84 @@
 
 void pulseEnable ()
 {
-   digitalWrite (LCD_E, HIGH) ;
-   delay(0.5); //  1/2 microsecond pause - enable pulse must be > 450ns
-   digitalWrite (LCD_E, LOW) ;
+	digitalWrite (LCD_E, HIGH) ;
+	delay(0.5); //  1/2 microsecond pause - enable pulse must be > 450ns
+	digitalWrite (LCD_E, LOW) ;
 }
 
 /*
-  send a byte to the lcd in two nibbles
-  before calling use SetChrMode or SetCmdMode to determine whether to send character or command
+send a byte to the lcd in two nibbles
+before calling use SetChrMode or SetCmdMode to determine whether to send character or command
 */
 void lcd_byte(char bits)
 {
-  digitalWrite (LCD_D4,(bits & 0x10)) ; 
-  digitalWrite (LCD_D5,(bits & 0x20)) ; 
-  digitalWrite (LCD_D6,(bits & 0x40)) ; 
-  digitalWrite (LCD_D7,(bits & 0x80)) ; 
-  pulseEnable();
+	digitalWrite (LCD_D4,(bits & 0x10)) ; 
+	digitalWrite (LCD_D5,(bits & 0x20)) ; 
+	digitalWrite (LCD_D6,(bits & 0x40)) ; 
+	digitalWrite (LCD_D7,(bits & 0x80)) ; 
+	pulseEnable();
 
-  digitalWrite (LCD_D4,(bits & 0x1)) ; 
-  digitalWrite (LCD_D5,(bits & 0x2)) ; 
-  digitalWrite (LCD_D6,(bits & 0x4)) ; 
-  digitalWrite (LCD_D7,(bits & 0x8)) ; 
-  pulseEnable();         
+	digitalWrite (LCD_D4,(bits & 0x1)) ; 
+	digitalWrite (LCD_D5,(bits & 0x2)) ; 
+	digitalWrite (LCD_D6,(bits & 0x4)) ; 
+	digitalWrite (LCD_D7,(bits & 0x8)) ; 
+	pulseEnable();         
 }
-
+/**
+To pass command to the LCD
+**/
 void SetCmdMode()
 {
-  digitalWrite (LCD_RS, 0); // set for commands
+	digitalWrite (LCD_RS, 0); // set for commands
 }
-
+/**
+To write text on the lcd
+*/
 void SetChrMode()
 {
-  digitalWrite (LCD_RS, 1); // set for characters
+	digitalWrite (LCD_RS, 1); // set for characters
 }
-
+/**
+Wrtie some text on screen
+**/
 void lcd_text(char *s)
 {
-  while(*s)
-   lcd_byte(*s++);
- }
-
+	while(*s)
+	lcd_byte(*s++);
+}
+/**
+Init the screen
+**/
 void lcd_init()
 {
 
-  // wiringPiSetupGpio () ; // use BCIM numbering
-   wiringPiSetup () ; // use wiring Pi numbering
-   // set up pi pins for output
-   pinMode (LCD_E,  OUTPUT);
-   pinMode (LCD_RS, OUTPUT);
-   pinMode (LCD_D4, OUTPUT);
-   pinMode (LCD_D5, OUTPUT);
-   pinMode (LCD_D6, OUTPUT);
-   pinMode (LCD_D7, OUTPUT);
-   
-   // initialise LCD
-   SetCmdMode(); // set for commands
-   lcd_byte(0x33); // full init
-   lcd_byte(0x32); // 4 bit mode
-   lcd_byte(0x28); // 2 line mode
-   lcd_byte(0x0C); // display on, cursor off, blink off
-   lcd_byte(0x01);  // clear screen
-   delay(3);        // clear screen is slow!
-}
+	// wiringPiSetupGpio () ; // use BCIM numbering
+	wiringPiSetup () ; // use wiring Pi numbering
+	// set up pi pins for output
+	pinMode (LCD_E,  OUTPUT);
+	pinMode (LCD_RS, OUTPUT);
+	pinMode (LCD_D4, OUTPUT);
+	pinMode (LCD_D5, OUTPUT);
+	pinMode (LCD_D6, OUTPUT);
+	pinMode (LCD_D7, OUTPUT);
 
+	// initialise LCD
+	SetCmdMode(); // set for commands
+	lcd_byte(0x33); // full init
+	lcd_byte(0x32); // 4 bit mode
+	lcd_byte(0x28); // 2 line mode
+	lcd_byte(0x0C); // display on, cursor off, blink off
+	lcd_byte(0x01);  // clear screen
+	delay(3);        // clear screen is slow!
+}
+/*
+Return at the begining of the line
+*/
 void goHome(){
-SetCmdMode(); // set for commands
-lcd_byte(lcd_Home);  // go home screen
-SetChrMode();
-delay(3);        // clear screen is slow!
+	SetCmdMode(); // set for commands
+	lcd_byte(lcd_Home);  // go home screen
+	SetChrMode();
+	delay(3);        // clear screen is slow!
 }
 
 
@@ -100,64 +110,95 @@ delay(3);        // clear screen is slow!
 **/
 int main (int argc, char** argv)
 {
- lcd_init();
- SetChrMode();
-int nbSecond=getCoundownValue();
-//time_t before = time(NULL);
-time_t whenItsComplete = time(NULL)+nbSecond;
-int remainingSeconds;
-char timestr[7];
-do {
-nbSecond=getCoundownValue();
-if(nbSecond>0){
-whenItsComplete = time(NULL)+nbSecond;
+	lcd_init();
+	SetChrMode();
+
+	int nbSecond;
+	int remainingSeconds;
+
+	time_t whenItsComplete ;
+	char timestr[7];
+
+	// Loop checking file
+	while(1){
+		nbSecond=getCoundownValue();
+		//printf("loop while 1\n");
+		// The countdown
+		whenItsComplete = time(NULL)+nbSecond;
+		do {
+			//printf("loop do\n");
+			remainingSeconds=whenItsComplete-time(NULL);
+			if(remainingSeconds>-1){
+			//	printf("if remaining>0\n");
+				int seconds=remainingSeconds%60;
+				int hours=remainingSeconds/3600;
+				int minutes=remainingSeconds/60%60;
+				sleep(1);
+				sprintf(timestr,"%02d:%02d:%02d",hours,minutes,seconds);
+				goHome();
+				lcd_text(timestr);
+			}
+			else{
+				goHome();
+				lcd_text("Expire    " );
+			}
+		} while ( remainingSeconds>0&& !isFilePresent() );
+
+		if(nbSecond==-2){
+			goHome();
+			lcd_text("Tele on      " );
+		}
+		else if(nbSecond==-1){
+			goHome();
+			lcd_text("Tele off      " );
+		}
+
+		sleep(2);
+	}
 }
 
-remainingSeconds=whenItsComplete-time(NULL);
-int seconds=remainingSeconds%60;
-int hours=remainingSeconds/3600;
-int minutes=remainingSeconds/60%60;
-sleep(1);
-sprintf(timestr,"%02d:%02d:%02d",hours,minutes,seconds);
-goHome();
-lcd_text(timestr);
- } while ( remainingSeconds>0 );
-printf("Compte à rebour expiré\n" );
+char filename[]="/tmp/scheduler/CD";
+
+/**
+* Check if file is present
+**/
+int isFilePresent(){
+	FILE * f=fopen (filename, "rb");
+	return f!=0;
 }
 
 /**
 Get coundown value.
 */
-
 int getCoundownValue(){
-int nbSecond=-1;
+	int nbSecond=-3;
 
-// read the content of the file
-char * buffer = 0;
-long length;
-char filename[]="/tmp/scheduler/CD";
+	// read the content of the file
+	char * buffer = 0;
+	long length;
 
-FILE * f=fopen (filename, "rb");
+	FILE * f=fopen (filename, "rb");
 
-if (f)
-{
-  fseek (f, 0, SEEK_END);
-  length = ftell (f);
-  fseek (f, 0, SEEK_SET);
-  /** The buffer stor the content of the file */
-  buffer = malloc (length);
-  if (buffer)
-  {
-    fread (buffer, 1, length, f);
-	 nbSecond = atoi(buffer);
-  }
-  fclose (f);
-}
-unlink (filename);
-if (buffer)
-{
- printf(buffer);  // start to process your data / extract strings here...
-}
- return nbSecond;
-
+	if (f)
+	{
+		fseek (f, 0, SEEK_END);
+		length = ftell (f);
+		fseek (f, 0, SEEK_SET);
+		/** The buffer stor the content of the file */
+		buffer = malloc (length);
+		if (buffer)
+		{
+			fread (buffer, 1, length, f);
+			nbSecond = atoi(buffer);
+		}
+		fclose (f);
+	}
+	if(f){
+		unlink (filename);
+	}
+	if (buffer)
+	{
+		printf(buffer);  // start to process your data / extract strings here...
+	}
+	return nbSecond;
 }
